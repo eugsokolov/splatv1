14c3a23532031beea875b6303830c8a0ae6024a0
eugene <eugsokolov@gmail.com>
2024-04-23 13:46:42 +0100

    change url

--- main.js
+++ main.js
@@ -748,7 +748,7 @@ async function main() {
         // location.href,
         // params.get("url") || "train.splat",
         // "https://huggingface.co/cakewalk/splat-data/resolve/main/",
-        "https://samconsidine.github.io/splatv1/output.splat",
+        "https://eugsokolov.github.io/splatv1/output.splat",
     );
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
